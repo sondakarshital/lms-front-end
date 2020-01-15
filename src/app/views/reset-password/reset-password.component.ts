8268ea5deb2187b09b4037e355a353281f35714f
Shital Shrikant Sondakar <sondakarshital@gmail.com>
2020-01-15 17:37:31 +0530

    some chnages

--- src/app/views/reset-password/reset-password.component.ts
+++ src/app/views/reset-password/reset-password.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import{ResetPasswordService} from '../../service/user-service/reset-password'
+import { ResetPasswordService } from '../../service/user-service/reset-password'
 
 @Component({
   selector: 'app-reset-password',
@@ -8,14 +8,15 @@ import{ResetPasswordService} from '../../service/user-service/reset-password'
   styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent implements OnInit {
-  password : "";
+  password: "";
+  confirmPassword: "";
   token = "";
   userid = "";
   message = "";
   showMessage = false;
   errorMessage = "";
   showErrMessage = false;
-  constructor(private activeRoute: ActivatedRoute,public resetPasswordService : ResetPasswordService) {
+  constructor(private activeRoute: ActivatedRoute, public resetPasswordService: ResetPasswordService) {
     activeRoute.queryParams
       .subscribe((params) => {
         console.log(params);
@@ -26,20 +27,47 @@ export class ResetPasswordComponent implements OnInit {
 
   ngOnInit() {
   }
-  changePassword(){
-    var user = {
-      password:this.password,
-      userid : this.userid
+  changePassword() {
+    console.log("inside changePassword",this.password,this.confirmPassword)
+    if (this.passwordCheck(this.password)&&this.passwordConfirmPasswordCheck(this.password,this.confirmPassword)){
+      var user = {
+        password: this.password,
+        userid: this.userid
+      }
+      this.resetPasswordService.newPassword(user, this.token).subscribe((data) => {
+        this.message = "Your password changed successfully ";
+        this.showMessage = true;
+        this.showErrMessage = false;
+        this.password =this.confirmPassword = "";
+      }, err => {
+        console.log("errorMessage", err.error.message)
+        this.errorMessage = err.error.message;
+        this.showErrMessage = true;
+        this.showMessage = false;
+      })
     }
-    this.resetPasswordService.newPassword(user,this.token).subscribe((data)=>{
-      this.message = "Your password changed successfully ";
-      this.showMessage = true;
-      this.showErrMessage = false;
-    },err=>{
-      console.log("errorMessage",err.error.message)
-      this.errorMessage =  err.error.message;
+  }
+  passwordCheck(password) {
+    if (password.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
+      return true;
+    } else {
+      console.log("here");
+      this.errorMessage = "Invalid password. Password must be at least 6 characters long, and contain a number."
       this.showErrMessage = true;
       this.showMessage = false;
-    })
+      return false;
+    }
+  }
+  passwordConfirmPasswordCheck(password, confirmPassword) {
+    if (password == confirmPassword) {
+      return true;
+    } else {
+      this.errorMessage = "Password and ConfirmPassword should be same";
+      this.showErrMessage = true;
+      this.showMessage = false;
+      return false;
+    }
+
   }
 }
+
